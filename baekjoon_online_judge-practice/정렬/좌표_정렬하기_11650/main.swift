f223d60b7175184ddb979782d077c832036697b2
YongwooKim <keepingitflow@gmail.com>
2021-09-02 10:21:58 +0900

    docs: BOJ-문제-단계별로풀어보기-정렬-좌표 정렬하기 11650 풀이완료 및 블로그 정리

--- baekjoon_online_judge-practice/정렬/좌표_정렬하기_11650/main.swift
+++ baekjoon_online_judge-practice/정렬/좌표_정렬하기_11650/main.swift
@@ -5,7 +5,17 @@
 //  Created by Yongwoo Marco on 2021/09/02.
 //
 
-import Foundation
+// https://www.acmicpc.net/problem/11650
 
-print("Hello, World!")
+let coordinateCount = Int(readLine()!)!
+var coordinates = [[Int]]()
 
+for _ in 0..<coordinateCount {
+    coordinates.append( readLine()!.split(separator: " ").map{ Int(String($0))! } )
+}
+
+coordinates.sorted { lhs, rhs -> Bool in
+    return lhs[0] < rhs[0] || (lhs[0] == rhs[0] && lhs[1] < rhs[1])
+}.forEach { coordinate in
+    print("\(coordinate[0]) \(coordinate[1])")
+}
