da6ee15715f8a90003ac145ba1ddd886eb625088
YongwooKim <keepingitflow@gmail.com>
2022-04-18 21:33:07 +0900

    docs: 프로그래머스 Lv1 x만큼 간격이 있는 n개의 숫자 해결

---
+++ programmers-phase/1단계/x만큼_간격이_있는_n개의_숫자/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  1단계/x만큼_간격이_있는_n개의_숫자
+//
+//  Created by Yongwoo Marco on 2022/04/18.
+//
+
+import Foundation
+
+// https://programmers.co.kr/learn/courses/30/lessons/12954
+
+func solution(_ x:Int, _ n:Int) -> [Int64] {
+    return (1...n).map{ Int64($0) * Int64(x) }
+}
+
+print(solution(2, 5)) // [2,4,6,8,10]
+print(solution(4, 3)) // [4,8,12]
+print(solution(-4, 2)) // [-4, -8]
