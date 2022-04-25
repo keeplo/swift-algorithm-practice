2abfd2ae1546cd1a27bf0605ade9440272f98621
YongwooKim <keepingitflow@gmail.com>
2022-04-25 18:40:17 +0900

    docs: 프로그래머스 Lv 1 최댓값과 최솟값 정리완료

---
+++ programmers-phase/1단계/최댓값과_최솟값/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  1단계/최댓값과_최솟값
+//
+//  Created by Yongwoo Marco on 2022/04/25.
+//
+
+// https://programmers.co.kr/learn/courses/30/lessons/12939
+
+import Foundation
+
+func solution(_ s:String) -> String {
+    let arr = s.components(separatedBy: " ").compactMap({ Int($0) })
+    return [arr.min(), arr.max()].compactMap { $0 }.map { String($0) }.joined(separator: " ")
+}
+
+print(solution("1 2 3 4"))  // "1 4"
+print(solution("-1 -2 -3 -4")) // "-4 -1"
+print(solution("-1 -1")) // "-1 -1"
