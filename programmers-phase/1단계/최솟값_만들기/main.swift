5424e9039a8b14b7a9d786596ae22bd3c87716e0
YongwooKim <keepingitflow@gmail.com>
2022-04-25 18:53:24 +0900

    docs: 프로그래머스 연습문제 Lv 1 최솟값 만들기 정리완료

---
+++ programmers-phase/1단계/최솟값_만들기/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  1단계/최솟값_만들기
+//
+//  Created by Yongwoo Marco on 2022/04/25.
+//
+
+// https://programmers.co.kr/learn/courses/30/lessons/12941
+
+import Foundation
+
+func solution(_ A:[Int], _ B:[Int]) -> Int {
+    return zip(A.sorted(by: >), B.sorted(by: <)).reduce(0) { $0 + $1.0 * $1.1 }
+}
+
+print(solution([1, 4, 2], [5, 4, 4])) // 29
+print(solution([1,2], [3,4])) // 10
+       
