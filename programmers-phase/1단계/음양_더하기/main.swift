a7fb255ae6852cd6d3a0dd1a74686d418f3010ef
YongwooKim <keepingitflow@gmail.com>
2022-05-03 18:19:27 +0900

    refactor: 프로그래머스 Lv 1 음양 더하기 수정완료
    월간 코드 챌린지 시즌 2

--- programmers-phase/1단계/음양_더하기/main.swift
+++ programmers-phase/1단계/음양_더하기/main.swift
@@ -8,14 +8,9 @@
 import Foundation
 
 // https://programmers.co.kr/learn/courses/30/lessons/76501
+
 func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
-    var sum = 0
-    
-    for index in 0..<signs.count {
-        sum += signs[index] ? absolutes[index] : -absolutes[index]
-    }
-    
-    return sum
+    return zip(absolutes, signs).reduce(0) { $0 + ($1.1 ? $1.0 : -$1.0) }
 }
 
 print(solution([4,7,12], [true,false,true])) // 9
