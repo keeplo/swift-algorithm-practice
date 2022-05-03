cb535569680c5e9491faa7feed60929adc2a424e
YongwooKim <keepingitflow@gmail.com>
2022-05-03 17:45:33 +0900

    refactor: 프로그래머스 Lv 1 로또의 최고순위와 최저 순위 정리완료
    2021 Dev-Matching: 웹 백엔드 개발자(상반기)

--- programmers-phase/1단계/로또의_최고_순위와_최저_순위/main.swift
+++ programmers-phase/1단계/로또의_최고_순위와_최저_순위/main.swift
@@ -8,14 +8,15 @@
 import Foundation
 
 // https://programmers.co.kr/learn/courses/30/lessons/77484
+
 func solution(_ lottos: [Int], _ win_nums: [Int]) -> [Int] {
     let zero = lottos.filter{ $0 == 0 }.count
     let match = lottos.filter{ win_nums.contains($0) }.count
  
-    return [match+zero, match].map({ $0 <= 1 ? 6 : 7-$0 })
+    return [match + zero, match].map { $0 <= 1 ? 6 : 7 - $0 }
 }
 
 
-print(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]))
-print(solution([0, 0, 0, 0, 0, 0], [38, 19, 20, 40, 15, 25]))
-print(solution([45, 4, 35, 20, 3, 9], [20, 9, 3, 45, 4, 35]))
+print(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19])) // [3, 5]
+print(solution([0, 0, 0, 0, 0, 0], [38, 19, 20, 40, 15, 25])) // [1, 6]
+print(solution([45, 4, 35, 20, 3, 9], [20, 9, 3, 45, 4, 35])) // [1, 1]
