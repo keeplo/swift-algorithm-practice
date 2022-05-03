dae7e01ee3c93518dbaa02eb8eadbecf091f5070
YongwooKim <keepingitflow@gmail.com>
2022-05-03 19:03:53 +0900

    refactor: 프로그래머스 Lv 1 최소직사각형 수정완료
    위클리 챌린지

--- programmers-phase/1단계/최소직사각형/main.swift
+++ programmers-phase/1단계/최소직사각형/main.swift
@@ -8,22 +8,19 @@
 import Foundation
 
 // https://programmers.co.kr/learn/courses/30/lessons/86491
+
 func solution(_ sizes:[[Int]]) -> Int {
     var widths = [Int](), height = [Int]()
     
     sizes.forEach { size in
-        if size[0] > size[1] {
-            widths.append(size[0])
-            height.append(size[1])
-        } else {
-            widths.append(size[1])
-            height.append(size[0])
-        }
+        let sorted = size.sorted(by: >)
+        widths.append(sorted[0])
+        height.append(sorted[1])
     }
 
     return widths.max()! * height.max()!
 }
 
-print(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
-print(solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]))
-print(solution([[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]))
+print(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))       // 4000
+print(solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]))  // 120
+print(solution([[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]))  // 133
