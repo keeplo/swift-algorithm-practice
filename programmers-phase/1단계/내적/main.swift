9635565befbe642721095472bde91a29cf136b74
YongwooKim <keepingitflow@gmail.com>
2022-05-03 17:58:19 +0900

    refactor: 프로그래머스 Lv 1 내적 수정완료
    월간 코드 챌린지 시즌 1

--- programmers-phase/1단계/내적/main.swift
+++ programmers-phase/1단계/내적/main.swift
@@ -8,8 +8,9 @@
 import Foundation
 
 // https://programmers.co.kr/learn/courses/30/lessons/70128
+
 func solution(_ a:[Int], _ b:[Int]) -> Int {
-    return (0..<a.count).reduce(0) { $0 + a[$1]*b[$1] }
+    return zip(a, b).reduce(0) { $0 + $1.0 * $1.1 }
 }
 
 print(solution([1,2,3,4], [-3,-1,0,2])) // 3
