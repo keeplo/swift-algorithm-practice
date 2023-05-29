8fc5bed1a861fa379cac49567e7524cc25b96b91
marco <keepingitflow@gmail.com>
2023-05-29 22:24:20 +0900

    chore: solved.ac - Class 1* - 음계 2920

---
+++ baekjoon_online_judge-practice/CLASS1/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  CLASS1
+//
+//  Created by Yongwoo Marco on 2023/05/29.
+//
+
+// https://www.acmicpc.net/problem/2920
+// 음계
+//let line = readLine()!.split(separator: " ").compactMap({ String($0) })
+//
+//let ascending = line.sorted(by: <)
+//let descending = line.sorted(by: >)
+//
+//if line == ascending {
+//	print("ascending")
+//} else if line == descending {
+//	print("descending")
+//} else {
+//	print("mixed")
+//}
