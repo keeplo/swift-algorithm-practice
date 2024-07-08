21551b40cfa42babd9d0853f841e2650f360f308
김용우 <marco@Marcos-MacBook.local>
2024-07-08 19:48:50 +0900

    chore: 프로젝트 생성 및 181942 풀이 추가

---
+++ Programmers/181942-문자열_섞기/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//: [181942-문자열_섞기](https://school.programmers.co.kr/learn/courses/30/lessons/181942?language=swift)
+//
+//  Created by 김용우 on 7/8/24.
+//
+
+import Foundation
+
+func solution(_ str1:String, _ str2:String) -> String {
+    return zip(str1, str2).map({ String($0) + String($1) }).joined()
+}
+
+print(solution("aaaaa", "bbbbb")) // "ababababab"
